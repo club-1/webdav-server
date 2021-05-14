5ba8a3f4b4d48bf6d9d8681bb5bb18d72db53792
n-peugnet <n.peugnet@free.fr>
2021-05-14 19:53:05 +0200

    continue testing sabre plugins

--- PosixPropertiesPlugin.php
+++ PosixPropertiesPlugin.php
@@ -1,16 +1,24 @@
 <?php
 
-use Sabre\DAV;
+use Sabre\DAV\INode;
+use Sabre\DAV\PropFind;
+use Sabre\DAV\Server;
+use Sabre\DAV\ServerPlugin;
 
-class PosixPropertiesPlugin extends DAV\ServerPlugin
+class PosixPropertiesPlugin extends ServerPlugin
 {
-    public function initialize(DAV\Server $server)
+    const NS_POSIXPROPS = 'http://club1.fr/posixprops/';
+
+    public function initialize(Server $server)
     {
         $server->on('propFind', [$this, 'propFind']);
+        $server->xml->namespaceMap[self::NS_POSIXPROPS] = 'p';
     }
-    public function propFind(DAV\PropFind $propFind, DAV\INode $node)
+    public function propFind(PropFind $propFind, INode $node)
     {
+        $ns = '{' . self::NS_POSIXPROPS . '}';
         // Hide all the things!
-        $propFind->set('{DAV:}ishidden', '1');
+        $propFind->set($ns . 'links', '1');
+        $propFind->set($ns . 'test', '1');
     }
 }
